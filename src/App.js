6fe2c7e7fe148ca77b7bc4cfc47188f49fa8a1d5
lillielove <lillielove@gmail.com>
2022-09-12 09:00:02 -0700

    created About section and Navigation bar in components folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import "./App.css";
+// App.js is center of the application; it's the root component, or wrapper component, that houses all other components
+// to effect any chance on the application, we need to either modify this file or add components inside it.d
 
+import React from "react"; //you need to import React in every component file
+import "./App.css";
+import About from "./components/About/index";
+import Navigation from "./components/Nav";
+
+// React components must always return a single parent JSX element - but it may have many children elements
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        //returns JSX, which can represent HTML in JS
+        <div>
+            <Navigation></Navigation>
+            <main>
+                <About></About>
+            </main>
         </div>
     );
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Navigation = () => {
+    const categories = [
+        {
+            name: "commercial",
+            description:
+                "Photos of grocery stores, food trucks, and other commercial products",
+        },
+        { name: "portraits", description: "Portraits of people in my life" },
+        { name: "food", description: "Delicious delicacies" },
+        {
+            name: "landscape",
+            description:
+                "Fields, farmhouses, waterfalls, and the beauty of nature",
+        },
+    ];
+
+    function categorySelected(name) {
+        console.log(`${name} selected`);
+    }
+
+    return (
+        <header>
+            <h2>
+                <a href="/">
+                    <span role="img" aria-label="camera">
+                        {" "}
+                        📸{" "}
+                    </span>{" "}
+                    Oh Snap!
+                </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <a href="#about">About Me</a>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                    {/* whenever we map over anything in JSX, the outermost element must have a key attribute */}
+                    {/* that's set to something unique (helps React keep track of items in the virtual DOM) */}
+                    {/* when you map over an array in a JSX expression, you should return only a single JSX element */}
+                    {/* like how you can only return a single element from a React component */}
+                    {categories.map((category) => (
+                        <li className="mx-1" key={category.name}>
+                            <span
+                                onClick={() => categorySelected(category.name)}
+                            >
+                                {category.name}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+// onClick is expecting a CB function declarataion; important that we wrap it in a function declaration rather than just calling
+// categorySelected(category.name), which would cause function to get called WHENEVER the component renders as well
+
+export default Navigation;
