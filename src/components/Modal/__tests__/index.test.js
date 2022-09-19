77accff9c01145a9d02f1ce8706083ca593df0dd
lillielove <lillielove@gmail.com>
2022-09-19 09:07:41 -0700

    more complete modal testing

--- src/components/Modal/__tests__/index.test.js
+++ src/components/Modal/__tests__/index.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Modal from "..";
 
@@ -14,22 +14,37 @@ const currentPhoto = {
     index: 1,
 };
 
-const toggleModal = (image, i) => {
-    setCurrentPhoto({ ...image, index: i });
-    // spread operaor used to add index:i key value pair to current photo state
-
-    // toggles from true to false
-    setIsModalOpen(!isModalOpen);
-};
+const mockToggleModal = jest.fn();
 
 afterEach(cleanup);
 
 describe("Modal is rendering", () => {
     it("renders", () => {
-        render(<Modal></Modal>);
+        render(
+            <Modal
+                onClose={mockToggleModal}
+                currentPhoto={currentPhoto}
+            ></Modal>
+        );
     });
     it("matches snapshot DOM node structure", () => {
-        const { asFragment } = render(<Modal />);
+        const { asFragment } = render(
+            <Modal onClose={mockToggleModal} currentPhoto={currentPhoto} />
+        );
         expect(asFragment()).toMatchSnapshot();
+    });
+});
+
+describe("click event", () => {
+    it("calls onClose handler", () => {
+        const { getByText } = render(
+            <Modal
+                onClose={mockToggleModal}
+                currentPhoto={currentPhoto}
+            ></Modal>
+        );
+        fireEvent.click(getByText("Close this modal"));
+
+        expect(mockToggleModal).toHaveBeenCalledTimes(1);
     });
 });
